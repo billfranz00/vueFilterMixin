e54335bd4d8d22c367a5c74094bcee48547104ad
billyf <Maggie91>
2018-10-23 17:26:41 -0700

    First Commit

---
+++ src/List.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Filters & Mixins 2</h1>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruit">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin } from './fruitMixin';
+
+    export default {
+        mixins: [fruitMixin]
+    }
+</script>
+
+<style lang="scss">
+    
+</style>
